202bfc39c0fa44f911327552e3d72f62392295ad
Zephied <galmiche1105@gmail.com>
2025-11-18 15:48:37 +0100

    ajout token rng pour appairage mqtt et refactorisation

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 #include "DHTesp.h"
 #include <WiFiManager.h>
 #include <PubSubClient.h>
+#include <iostream>
+#include <string>
+#include <random>
+#include <Preferences.h>
  
 WiFiManager wm;
 DHTesp dht;
@@ -9,14 +13,32 @@ DHTesp dht;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-long lastMSG = 0;
+unsigned long lastMSG = 0;
 float lastTemperature = 0.0;
 float lastHumidity = 0.0;
+String tempTopic = "WeatherB2/temperature/";
+String humTopic = "WeatherB2/humidity/";
+
+std::string generateRandomString(size_t length) {
+    std::string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    std::random_device rd;
+    std::mt19937 generator(rd()); // Générateur aléatoire
+    std::uniform_int_distribution<> distribution(0, characters.size() - 1);
+
+    std::string result;
+    for (size_t i = 0; i < length; ++i) {
+        result += characters[distribution(generator)];
+    }
+
+    return result;
+}
+
+String mqtt_id;
 
 void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    if (client.connect("WeatherB2")) {
+    if (client.connect(("WeatherB2-"+mqtt_id).c_str())) {
       Serial.println("connected");
     } else {
       Serial.print("failed, rc=");
@@ -29,28 +51,48 @@ void reconnect() {
 }
 
 void setup() {
+  // wm.resetSettings(); // Uncomment to reset Wi-Fi settings
 
+  Preferences prefs;
+  prefs.begin("mqtt", false);
+
+  if (prefs.isKey("id")) {
+    String stored = prefs.getString("id");
+    mqtt_id = stored;
+
+    Serial.print("MQTT id loaded from NVS: ");
+    Serial.println(mqtt_id);
+  } else {
+    std::string rand = generateRandomString(10);
+    mqtt_id = String(rand.c_str());
+    prefs.putString("id", mqtt_id);
+
+    Serial.print("MQTT id generated and saved to NVS: ");
+    Serial.println(mqtt_id);
+  }
+  prefs.end();
+
+  String s = "<p>code d'appairage MQTT: "+mqtt_id+"</p>";
+  static WiFiManagerParameter mqttid(s.c_str());
+  wm.addParameter(&mqttid);
   WiFi.mode(WIFI_STA);
-
+  
   Serial.begin(115200);
-
   dht.setup(25, DHTesp::DHT11);
 
-  delay(1000);
   Serial.println();
-
   Serial.println("Tentative de connexion au réseau Wi-Fi...");
 
-    if (!wm.autoConnect()) {
-        Serial.println("Erreur de connexion au réseau Wi-Fi.");
-    } else {
-        Serial.println("Connexion au réseau Wi-Fi réussie !");
-        Serial.print("Adresse IP : ");
-        Serial.println(WiFi.localIP());
-    }
+  if (!wm.autoConnect()) {
+    Serial.println("Erreur de connexion au réseau Wi-Fi.");
+  } else {
+    Serial.println("Connexion au réseau Wi-Fi réussie !");
+    Serial.print("Adresse IP : ");
+    Serial.println(WiFi.localIP());
+  }
 
-    client.setServer("broker.emqx.io", 1883);
-    client.connect("WeatherB2");
+  client.setServer("broker.emqx.io", 1883);
+  reconnect();
 }
 
 void loop() {
@@ -64,12 +106,19 @@ void loop() {
   float humidity = dht.getHumidity();
   float temperature = dht.getTemperature();
 
-  if (currentTime - lastMSG > 60000 || abs(temperature - lastTemperature) >= 0.5 || abs(humidity - lastHumidity) >= 1.0) {
+  if (currentTime - lastMSG > 60000) {
     lastMSG = currentTime;
     lastTemperature = temperature;
     lastHumidity = humidity;
 
-    client.publish("WeatherB2/temperature", String(lastTemperature).c_str());
-    client.publish("WeatherB2/humidity", String(lastHumidity).c_str());
+    client.publish((tempTopic+mqtt_id).c_str(), String(lastTemperature).c_str());
+    client.publish((humTopic+mqtt_id).c_str(), String(lastHumidity).c_str());
+
+  } else if (std::abs(temperature - lastTemperature) >= 0.5 || std::abs(humidity - lastHumidity) >= 5.0) {
+    lastTemperature = temperature;
+    lastHumidity = humidity;
+
+    client.publish((tempTopic+mqtt_id).c_str(), String(lastTemperature).c_str());
+    client.publish((humTopic+mqtt_id).c_str(), String(lastHumidity).c_str());
   }
 }
