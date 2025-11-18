1d439b6d827fd1faf711ffd8e2580762831c7756
Zephied <galmiche1105@gmail.com>
2025-11-18 09:08:04 +0100

    init new repo for iot only

---
+++ src/main.cpp
@@ -0,0 +1,75 @@
+#include <Arduino.h>
+#include "DHTesp.h"
+#include <WiFiManager.h>
+#include <PubSubClient.h>
+ 
+WiFiManager wm;
+DHTesp dht;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+long lastMSG = 0;
+float lastTemperature = 0.0;
+float lastHumidity = 0.0;
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("Nathan")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+
+  WiFi.mode(WIFI_STA);
+
+  Serial.begin(115200);
+
+  dht.setup(25, DHTesp::DHT11);
+
+  delay(1000);
+  Serial.println();
+
+  Serial.println("Tentative de connexion au réseau Wi-Fi...");
+
+    if (!wm.autoConnect()) {
+        Serial.println("Erreur de connexion au réseau Wi-Fi.");
+    } else {
+        Serial.println("Connexion au réseau Wi-Fi réussie !");
+        Serial.print("Adresse IP : ");
+        Serial.println(WiFi.localIP());
+    }
+
+    client.setServer("broker.emqx.io", 1883);
+    client.connect("Nathan");
+}
+
+void loop() {
+  long currentTime = millis();
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  float humidity = dht.getHumidity();
+  float temperature = dht.getTemperature();
+
+  if (currentTime - lastMSG > 60000 || abs(temperature - lastTemperature) >= 0.5 || abs(humidity - lastHumidity) >= 1.0) {
+    lastMSG = currentTime;
+    lastTemperature = temperature;
+    lastHumidity = humidity;
+
+    client.publish("Nathan/temperature", String(lastTemperature).c_str());
+    client.publish("Nathan/humidity", String(lastHumidity).c_str());
+  }
+}
